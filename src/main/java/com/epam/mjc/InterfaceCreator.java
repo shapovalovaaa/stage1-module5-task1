d8fc8601412e0315e95bc427f84c64fd5f104b2e
shapovalovaaa <nastyashpovalova70@gmail.com>
2023-08-05 20:58:33 +0300

    feat: solve task

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,5 +1,6 @@
 package com.epam.mjc;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
@@ -7,26 +8,38 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InterfaceCreator {
 
     public Predicate<List<String>> isValuesStartWithUpperCase() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return x -> x.stream()
+                .allMatch(word -> Character.isUpperCase(word.charAt(0)));
     }
 
     public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return x -> x.addAll(x.stream()
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList())
+        );
     }
 
     public Supplier<List<String>> filterCollection(List<String> values) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return () -> values.stream()
+                .filter(s -> Character.isUpperCase(s.charAt(0)))
+                .filter(s -> s.endsWith("."))
+                .filter(s -> s.split(" ").length > 3)
+                .collect(Collectors.toList());
     }
 
     public Function<List<String>, Map<String, Integer>> stringSize() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return x -> x.stream()
+                .collect(Collectors.toMap(Function.identity(), String::length));
     }
 
     public BiFunction<List<Integer>, List<Integer>, List<Integer>> concatList() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return (list1, list2) -> Stream.concat(list1.stream(), list2.stream())
+                .collect(Collectors.toList());
     }
 }
